dc029735655103d5a807dc7c74cec2eeb332f980
HenryMacTiery <machariawaribu@gmail.com>
2021-01-14 12:35:44 +0300

    Completed basics: adding todos, deleting. No persistence

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background: rgb(187, 183, 183);
+  color: fff;
+  text-align: center;
+  padding: 10px;
+}
+.header a {
+  color: #ffffff;
+  padding-right: 5px;
+}
+</style>
