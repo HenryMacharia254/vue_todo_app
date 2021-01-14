b1f875d11b5efaaab8d740e0bdf4e4dfa111df0d
HenryMacTiery <machariawaribu@gmail.com>
2021-01-14 17:25:32 +0300

    Added getting notes from jsonplaceholder fake REST API

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
 
 export default {
   name: "App",
@@ -20,25 +21,17 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo two",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false,
-        },
-      ],
+      todos: [],
     };
   },
+  // special method "created". Runs right away. Make requests here. Can use getch api or axios
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
+
   methods: {
     deleteToDo(id) {
       this.todos = this.todos.filter((todo) => todo.id != id);
