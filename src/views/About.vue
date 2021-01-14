b0e3183e477f15822fee686ea7fb8632a13308b8
HenryMacTiery <machariawaribu@gmail.com>
2021-01-14 18:33:11 +0300

    Vue Routing

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h3>About page</h3>
+    <p>This is a ToDo List app version 1.0.0.</p>
+    <!-- <p>Technologies used:</p>
+    <ul>
+      <li>Vue</li>
+      <li>Vue CLI</li>
+      <li>Vue Router</li>
+      <li>JSONPlaceholder fake REST API</li>
+      <li>Axios</li>
+      <li>
+        uuid **Momentarily before use of id property given by JSONPlaceholder**
+      </li>
+    </ul> -->
+  </div>
+</template>
